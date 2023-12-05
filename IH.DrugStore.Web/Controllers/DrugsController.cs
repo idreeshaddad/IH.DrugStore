4e63550adb5315438b88e1d1b40ed837407fa32a
Idrees Haddad <gelnox@live.com>
2023-12-05 19:48:06 +0300

    Added Drugs controller and CRUD actions and views

---
+++ IH.DrugStore.Web/Controllers/DrugsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IH.DrugStore.Web.Controllers
+{
+    public class DrugsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
